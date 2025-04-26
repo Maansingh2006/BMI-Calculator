ce8e5e8803085e0524acbb26b6faeb9469ba8227
MankiratGurna <mankiratgurna2595@gmail.com>
2025-04-26 10:33:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,4 +60,24 @@ document.addEventListener('DOMContentLoaded', function() {
                         }
                     }
                 },
+
+    plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                const index = context.dataIndex;
+                                if (index === 0) return 'BMI < 18.5';
+                                if (index === 1) return '18.5 - 24.9';
+                                if (index === 2) return '25 - 29.9';
+                                if (index === 3) return 'BMI ≥ 30';
+                                return '';
+                            }
+                        }
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            }
+        };
   
